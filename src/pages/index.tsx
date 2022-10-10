2394c5e709797934514238968e4b8d756f0ff7cd
Sacha Rodier <rodier@neoskop.de>
2022-10-10 23:44:28 +0200

    removes auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -62,7 +62,7 @@ const Home: NextPage = () => {
 export default Home;
 
 const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
+  // const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
 
   const { data: sessionData } = useSession();
 
@@ -73,9 +73,9 @@ const AuthShowcase: React.FC = () => {
           Logged in as {sessionData?.user?.name}
         </p>
       )}
-      {secretMessage && (
+      {/* {secretMessage && (
         <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
+      )} */}
       <button
         className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
         onClick={sessionData ? () => signOut() : () => signIn()}
