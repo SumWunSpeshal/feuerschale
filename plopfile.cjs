4bf7c33f05a6649c57a9f96c6e4a9b1bff8a70dc
Sacha Rodier <rodier@neoskop.de>
2022-12-03 21:40:52 +0100

    adds plop

---
+++ plopfile.cjs
@@ -0,0 +1,47 @@
+const Choices = {
+  component: "component",
+  page: "page",
+};
+
+module.exports = (plop) => {
+  plop.setGenerator("generate", {
+    description: "generate a new component",
+    prompts: [
+      {
+        type: "list",
+        name: "type",
+        message: "What do you want to generate?",
+        choices: Object.values(Choices),
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Assign a name to your new entity",
+      },
+    ],
+    actions: (data) => {
+      switch (data.type) {
+        case Choices.component:
+          return [
+            {
+              type: "add",
+              path: "src/components/{{pascalCase name}}.tsx",
+              templateFile: "plop-templates/component.hbs",
+            },
+          ];
+        case Choices.page:
+          return [
+            {
+              type: "add",
+              path: "pages/{{kebabCase name}}.page.tsx",
+              templateFile: "plop-templates/page.hbs",
+            },
+          ];
+        default:
+          throw new Error(
+            `Missing 'type'. A 'type' must be assigned to know what to generate!`
+          );
+      }
+    },
+  });
+};
