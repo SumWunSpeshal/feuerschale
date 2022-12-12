8a669813a9180e01513bf47c07916b648c857cd9
Sacha Rodier <rodier@neoskop.de>
2022-12-12 18:04:05 +0100

    improves readability

--- src/components/CheckInput.tsx
+++ src/components/CheckInput.tsx
@@ -1,4 +1,5 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import clsx from "clsx";
 import { ForwardedRef, forwardRef } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 import { Error } from "./Error";
@@ -25,12 +26,20 @@ export const CheckInput = forwardRef(function CheckInput(
         className="flex cursor-pointer items-center gap-3"
       >
         <input type="checkbox" {...rest} ref={ref} className="peer sr-only" />
-        <div className="square-8 flex items-center justify-center rounded-lg border-2 border-black shadow-brutal outline-none transition-shadow peer-checked:[--icon-display:block] peer-hover:shadow-brutal-lg peer-focus:shadow-brutal-lg">
+        <div
+          className={clsx(
+            "square-8 flex items-center justify-center rounded-lg border-2 border-black shadow-brutal outline-none transition-shadow",
+            "peer-checked:[--icon-display:block] peer-hover:shadow-brutal-lg peer-focus:shadow-brutal-lg"
+          )}
+        >
           <div style={{ display: "var(--icon-display, none)" }}>
             <Icon icon={faXmark} />
           </div>
         </div>
-        <span>{label || name}</span>
+        <span>
+          {label || name}
+          {required ? " *" : ""}
+        </span>
       </label>
 
       <Error>{error}</Error>
