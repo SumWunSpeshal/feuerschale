6f8e7f7ee7065ad21a828d3dccf9d0a464094928
SumWunSpeshal <mail@srodier.com>
2022-12-23 17:15:39 +0100

    adds styling for signin page

--- src/pages/auth/signin.page.tsx
+++ src/pages/auth/signin.page.tsx
@@ -7,6 +7,7 @@ import FacebookSvg from "public/img/facebook.svg";
 import GoogleSvg from "public/img/google.svg";
 import { BrutalElevation } from "src/components/BrutalElevation";
 import { Button } from "src/components/Button";
+import { Container } from "src/components/Container";
 import { Global } from "src/components/Global";
 
 type SignIn = Record<
@@ -23,43 +24,45 @@ const SignIn: NextPage<{ providers: SignIn }> = ({ providers }) => {
   return (
     <Global>
       <div className="grid grow place-items-center bg-amber-100">
-        <div className="relative max-w-xs">
-          <BrutalElevation className="!top-4 !h-[calc(100%-1rem)]">
-            <fieldset className="relative z-10 rounded-3xl border-2 border-black bg-white p-6 text-center">
-              <legend className="rounded-md border-2 border-black bg-white px-3 text-2xl">
-                <strong>Login</strong>
-              </legend>
-              <div className="mb-4">
-                <span className="text-gray-600">
-                  Melde Dich mit einem Deiner bestehenden Konten an:{" "}
-                </span>
-              </div>
-              <div className="space-y-4">
-                {Object.values(providers ?? {}).map((provider) => (
-                  <div key={provider.name}>
-                    <Button
-                      className="w-full"
-                      onClick={() =>
-                        signIn(provider.id, {
-                          callbackUrl: "/",
-                        })
-                      }
-                    >
-                      <div className="flex grow items-center justify-center gap-3">
-                        <Image
-                          src={config[provider.name as keyof typeof config]}
-                          alt={`${provider.name} Provider`}
-                          width={28}
-                        />
-                        <span>{provider.name}</span>
-                      </div>
-                    </Button>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
-          </BrutalElevation>
-        </div>
+        <Container>
+          <div className="relative max-w-xs">
+            <BrutalElevation className="!top-4 !h-[calc(100%-1rem)]">
+              <fieldset className="relative z-10 rounded-3xl border-2 border-black bg-white p-6 text-center">
+                <legend className="rounded-md border-2 border-black bg-white px-3 text-2xl">
+                  <strong>Login</strong>
+                </legend>
+                <div className="mb-4">
+                  <span className="text-gray-600">
+                    Melde Dich mit einem Deiner bestehenden Konten an:{" "}
+                  </span>
+                </div>
+                <div className="space-y-4">
+                  {Object.values(providers ?? {}).map((provider) => (
+                    <div key={provider.name}>
+                      <Button
+                        className="w-full"
+                        onClick={() =>
+                          signIn(provider.id, {
+                            callbackUrl: "/",
+                          })
+                        }
+                      >
+                        <div className="flex grow items-center justify-center gap-3">
+                          <Image
+                            src={config[provider.name as keyof typeof config]}
+                            alt={`${provider.name} Provider`}
+                            width={28}
+                          />
+                          <span>{provider.name}</span>
+                        </div>
+                      </Button>
+                    </div>
+                  ))}
+                </div>
+              </fieldset>
+            </BrutalElevation>
+          </div>
+        </Container>
       </div>
     </Global>
   );
