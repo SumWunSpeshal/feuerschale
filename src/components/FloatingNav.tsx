05f73be2f0d689a4d731fadb877864c7df14bc81
SumWunSpeshal <mail@srodier.com>
2022-12-26 12:44:22 +0100

    fixes z-index

--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -10,18 +10,18 @@ export function FloatingNav(props: FloatingNavProps) {
   const { hrefToListView } = props;
 
   return (
-    <aside className="fixed bottom-4 -left-1 -translate-x-full animate-floating-nav">
+    <aside className="fixed bottom-4 -left-1 z-50 -translate-x-full animate-floating-nav">
       <div className="flex flex-col gap-3 pl-5">
         {hrefToListView && (
           <div>
             <Button.Anchor href={hrefToListView} className="group">
-              <Icon icon={faList} size={24} />
+              <Icon icon={faList} size={16} />
             </Button.Anchor>
           </div>
         )}
         <div>
           <Button.Anchor href="/" className="group">
-            <Icon icon={faHomeLgAlt} size={24} />
+            <Icon icon={faHomeLgAlt} size={16} />
           </Button.Anchor>
         </div>
       </div>
